62ec22567cfddfcf11f7d7c65888e35f8a65e058
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-23 01:40:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,6 +28,16 @@ class Dog
     DB[:conn].execute(sql)
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+
+  def self.find_by_name
+  end
+
   def save
     if self.id
       self.update
