7f3983d25ac63fb9e7db368d19b6948e6c1822eb
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-23 01:42:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,7 +35,14 @@ class Dog
     self.new(id: id, name: name, breed: breed)
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name)
   end
 
   def save
